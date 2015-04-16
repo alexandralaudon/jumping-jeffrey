69e3b5e41fd6af62736c4d8bd7429d2edacab20b
Alex <alexandralaudon@gmail.com>
2015-04-16 14:20:06 -0600

    Added Messages#new && updated CSS

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,8 +5,21 @@ class MessagesController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      flash[:notice] = "PLEASE DON'T MESS UP THE SYSTEM."
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :note)
+  end
+
 
 
 end
