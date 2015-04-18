4632d9c0e170705d451e0f541f428ba8229d64a3
Alex <alexandralaudon@gmail.com>
2015-04-18 08:14:58 -0600

    Deleted route to adding messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get 'add_message', to: 'messages#new'
-  post 'add_message', to: 'messages#create'
+  # get 'add_message', to: 'messages#new'
+  # post 'add_message', to: 'messages#create'
 
 end
